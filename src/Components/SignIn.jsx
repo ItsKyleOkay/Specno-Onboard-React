7f60d9a57478e8b9afac2e6bfb4f70573c135aa1
Kyle Kretschmer <krtkyl004@myuct.ac.za>
2022-06-28 12:06:20 +0200

    sign in page changes

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
+import classes from "./SignIn.module.css";
 import firebase from "firebase";
 
 const SignIn = () => {
@@ -73,67 +74,67 @@ const SignIn = () => {
     }
   };
   return (
-    <div className="mt-8rm -rf .git ">
-      <h1 className="text-5xl text-blue-500 mb-2 text-center font-bold">
-        {" "}
-        Specno{" "}
-      </h1>
-      <h2 className="text-3xl mb-2 text-center font-bold">Sign In</h2>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && (
-          <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
-            {error}
-          </div>
-        )}
-        <form className="">
-          <label htmlFor="userEmail" className="block">
-            Email:
-          </label>
-          <input
-            type="email"
-            className="my-1 p-1 w-full"
-            name="userEmail"
-            value={email}
-            placeholder="E.g: name123@gmail.com"
-            id="userEmail"
-            onChange={(event) => onChangeHandler(event)}
-          />
-          <label htmlFor="userPassword" className="block">
-            Password:
-          </label>
-          <input
-            type="password"
-            className="mt-1 mb-3 p-1 w-full"
-            name="userPassword"
-            value={password}
-            placeholder="Your Password"
-            id="userPassword"
-            onChange={(event) => onChangeHandler(event)}
-          />
-          <button
-            className="bg-green-400 hover:bg-green-500 w-full py-2 text-white"
-            onClick={(event) => {
-              signInWithEmailAndPasswordHandler(event, email, password);
-            }}
-          >
-            Sign in
-          </button>
-        </form>
-        <p className="text-center my-3">
-          <br />
-          <button
-            onClick={() => navigate("/password-reset")}
-            className="text-blue-500 hover:text-blue-600"
-          >
-            Forgot Password?
-          </button>
-        </p>
-        <img
-          src="https://uploads-ssl.webflow.com/5f479f688ac92ece1c89402b/5f74a4cc0b72d66c9c82bc99_Group%2029017.svg"
-          loading="lazy"
-          alt=""
-          class="image-155 rocket-footer"
-        ></img>
+    <div className={classes.contentcontainer}>
+      <div className={classes.row}>
+        <div className={classes.leftpanel}>
+          <h1 className={classes.welcome}>
+            Hello! welcome to Specno's Onboarding platform
+          </h1>
+          <img
+            src="https://uploads-ssl.webflow.com/5f479f688ac92ece1c89402b/5f74a4cc0b72d66c9c82bc99_Group%2029017.svg"
+            loading="lazy"
+            alt=""
+            class={classes.rocket}
+          ></img>
+        </div>
+        <div className={classes.rightpanel}>
+          <h1 className={classes.headingspecno}> Specno </h1>
+          <h2 className={classes.signinhead}>Sign In</h2>
+
+          <form className="">
+            <label htmlFor="userEmail" className={classes.headerdetails}>
+              Email:
+            </label>
+            <input
+              type="email"
+              className={classes.useremail}
+              name="userEmail"
+              value={email}
+              placeholder="E.g: name123@gmail.com"
+              id="userEmail"
+              onChange={(event) => onChangeHandler(event)}
+            />
+            <label htmlFor="userPassword" className={classes.headerdetails}>
+              Password:
+            </label>
+            <input
+              type="password"
+              className={classes.useremail}
+              name="userPassword"
+              value={password}
+              placeholder="Your Password"
+              id="userPassword"
+              onChange={(event) => onChangeHandler(event)}
+            />
+            <button
+              className={classes.buttonSignin}
+              onClick={(event) => {
+                signInWithEmailAndPasswordHandler(event, email, password);
+              }}
+            >
+              Sign in
+            </button>
+          </form>
+          <p className="text-left ">
+            <br />
+            <button
+              onClick={() => navigate("/password-reset")}
+              className={classes.forgotpassword}
+            >
+              Forgot Password?
+            </button>
+          </p>
+        </div>
       </div>
     </div>
   );

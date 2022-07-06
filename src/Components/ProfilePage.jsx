32f1b21e63874238a4991f440d2a20a5d33f0a5b
Muhammad-Jaffer <3936429@myuct.ac.za>
2022-07-07 00:21:19 +0200

    Changes to home and content

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 import { UserContext } from "../providers/UserProvider";
 import { auth } from "../firebase";
-import RegisterCourse from "../EmployeeMainPage/ContentQuiz";
-import classes from "./ProfilePage.module.css";
 import { useNavigate } from "react-router-dom";
+import Logo from "../Styles/img/logo.png";
+import Rocket from "../Styles/img/rocket.png";
+import '../Styles/style.css';
+import '../Styles/bootstrap/css/bootstrap.min.css';
+
 
 const ProfilePage = () => {
   const user = useContext(UserContext);
@@ -12,35 +15,47 @@ const ProfilePage = () => {
 
   return (
     <div>
-      <span className={classes.header}>
-        <div className={classes.logo}>Specno</div>
-        <nav>
+    <header id="header" className="fixed-top d-flex align-items-center">
+      <div className="container d-flex justify-content-between align-items-center">
+  
+        <div className="logo">
+          <a href="index.html"><img src={Logo} alt="" className="img-fluid" /></a>
+          <h1><a href="index.html">Specno</a></h1>
+        </div>
+  
+        <nav id="navbar" className="navbar">
           <ul>
-            <li>
-              <a href="/profile">Home</a>
-            </li>
-            <li>
-              <a href="/specno-quiz-content">Content</a>
-            </li>
-            <li>
-              <a href="/specno-quiz">Quizzes</a>
-            </li>
-            <li>
-              <a href="/specno-blog">Blog</a>
-            </li>
-            <li>
-              <a href="/leaderboard">Leaderboard</a>
-            </li>
+            <li><a className="active " href="/profile">Home</a></li>
+            <li><a href="/specno-quiz-content">Content</a></li>
+            <li><a href="/specno-quiz">Quizzes</a></li>
+            <li><a href="/leaderboard">Leaderboard</a></li>
           </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
-      </span>
-      <div className={classes.welcome}>
-        <h1>Welcome!</h1>
-        <h2>
-          Designing And Building World Class Digital Products Specno Partners
-          With You To Validate, Design And Develop Industry Leading Solutions
-        </h2>
+  
       </div>
+    </header>
+  
+    <section className="hero-section" id="hero">
+
+  
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-12 hero-text-image">
+            <div className="row">
+              <div className="col-lg-8 text-center text-lg-start">
+                <h1 data-aos="fade-right">Designing And Building World Class Digital Products</h1>
+                <p className="mb-5" data-aos="fade-right" data-aos-delay="100">Specno Partners With You To Validate, Design And Develop Industry Leading Solutions</p>
+              </div>
+              <div className="col-lg-8 iphone-wrap">
+                <img src={Rocket} alt="Image" className="phone-1" data-aos="fade-right" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+    </section>
       <div className="mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
         <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
           <div className="border border-blue-300"></div>
@@ -59,6 +74,8 @@ const ProfilePage = () => {
           Sign out
         </button>
       </div>
+      <script src="../Styles/bootstrap/js/bootstrap.bundle.min.jss"></script>
+      <script src="../Styles/main.js"></script>
     </div>
   );
 };
--- src/EmployeeMainPage/Quiz.js
+++ src/EmployeeMainPage/Quiz.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
 import classes from "./Quiz.module.css";
 import firebase from "firebase";
+import contentPic from "../Styles/img/content.jpg";
+import '../Styles/style.css';
+import '../Styles/bootstrap/css/bootstrap.min.css';
 
 const Quiz = () => {
   const [loading, setLoading] = useState(true);
@@ -57,14 +60,31 @@ const Quiz = () => {
           </ul>
         </nav>
       </span>
+      <section id="courses" className="courses">
+            <div className="container" data-aos="fade-up">
+      
+        <div className="row" data-aos="zoom-in" data-aos-delay="100">
+
+          
       {posts.map((post) => (
-        <div className={classes.column}>
-          <div className={classes.card}>
-            <h1 className={classes.contentname}>{post.Name}</h1>
-            <h1 className={classes.descr}>{post.Info}</h1>
-          </div>
-        </div>
+        <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+        <div className="course-item">
+              <img src={contentPic} className="img-fluid" alt="..." />
+              <div className="course-content">
+                <div className="d-flex justify-content-between align-items-center mb-3">
+                  <h4>Web Development</h4>
+                </div>
+
+                <h3><a href="#">{post.Name}</a></h3>
+                <p>{post.Info}</p>
+              </div>
+            </div>
+            </div>
       ))}
+      
+          </div>
+          </div>
+    </section>
     </div>
   );
 };

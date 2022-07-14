8c86a0f5a93cb6c44552fea2d79c7147ed8bb6b9
Muhammad-Jaffer <3936429@myuct.ac.za>
2022-07-15 01:48:54 +0200

    Style changes

--- src/EmployeeMainPage/Quiz.js
+++ src/EmployeeMainPage/Quiz.js
@@ -40,16 +40,16 @@ const Quiz = () => {
         <div className="container" data-aos="fade-up">
           <div className="row" data-aos="zoom-in" data-aos-delay="100">
             {posts.map((post) => (
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-                <div className="course-item">
-                  <img src={contentPic} className="img-fluid" alt="..." />
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0 rounded">
+                <div className="rounded course-item">
+                  <img src={contentPic} className="img-fluid rounded-top" alt="..." />
                   <div className="course-content">
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h4>Web Development</h4>
                     </div>
 
                     <button
-                      className="d-flex justify-content-between align-items-center mb-3 color Blue"
+                      className="d-flex justify-content-between align-items-center mb-3 color Blue fw-bold"
                       onClick={() => {
                         /* 1. Navigate to the Details route with params */
                         navigate("/specno-quiz/data", {
